fa630a7e23c62638d81e2812c5af0e4d1d09e606
Alexandre Hallaine <ahallain@student.42.fr>
2020-12-26 03:58:13 +0100

    fix: setup.sh stay running

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #start kubernetes
 microk8s start
 microk8s enable dns dashboard
-microk8s kubectl wait --for=condition=AVAILABLE -n kube-system deployment.apps/kubernetes-dashboard
+microk8s kubectl -n kube-system wait --for=condition=AVAILABLE deployment.apps/kubernetes-dashboard
 
 #build docker and start a deployment 
 for d in srcs/* ; do
@@ -12,10 +12,7 @@ for d in srcs/* ; do
 	microk8s kubectl apply -f $d/deployment.yaml
 done
 
-#display token
+#display token and endpoint
 token=$(microk8s kubectl -n kube-system get secret | grep default-token | cut -d " " -f1)
 microk8s kubectl -n kube-system describe secret $token | grep token:
-
-#open proxy
-# xdg-open https://127.0.0.1:10443/
-microk8s kubectl port-forward -n kube-system service/kubernetes-dashboard 10443:443
+microk8s kubectl -n kube-system describe service/kubernetes-dashboard | grep Endpoints:
