bde8aab7acff2a422868dcf42e9bdd33f98b06af
Alexandre Hallaine <ahallain@student.42.fr>
2020-12-26 18:34:53 +0100

    add: metallb support

--- setup.sh
+++ setup.sh
@@ -1,15 +1,18 @@
 #start kubernetes
 microk8s start
+ip=$(hostname -I | cut -d " " -f1)
+microk8s enable metallb:$ip-$ip
 microk8s enable dns dashboard
 microk8s kubectl -n kube-system wait --for=condition=AVAILABLE deployment.apps/kubernetes-dashboard
 
 #build docker and start a deployment 
-for d in srcs/* ; do
-	docker build $d -t $d:local
-	docker save $d > temp.tar
+for path in srcs/* ; do
+	folder=$(echo "$path" | cut -d "/" -f2)
+	docker build $path -t $folder:local
+	docker save $folder > temp.tar
 	microk8s ctr image import temp.tar
 	rm -f temp.tar
-	microk8s kubectl apply -f $d/deployment.yaml
+	microk8s kubectl apply -f $path/deployment.yaml
 done
 
 #display token and endpoint
