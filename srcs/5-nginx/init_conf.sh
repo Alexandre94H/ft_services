07dbfd2a460c6cb31efc22eaa773cffa270e7037
Alexandre Hallaine <ahallain@student.42.fr>
2021-01-20 19:57:47 +0100

    add: nginx cert, website and wordpress redirection

---
+++ srcs/5-nginx/init_conf.sh
@@ -0,0 +1,32 @@
+echo "server {
+        listen 80;
+
+        location / {
+                return 404;
+        }
+
+        location = /404.html {
+                internal;
+        }
+        return 301 https://\$host\$request_uri;
+}
+
+server {
+        listen 443 ssl;
+		
+        ssl_certificate     /etc/ssl/private/nginx.cert.pem;
+        ssl_certificate_key /etc/ssl/private/nginx.key.pem;
+
+        root /var/www/react;
+
+        index index.html;
+
+        location = /wordpress {
+            return 307 http://$IP:$WPPORT;
+        }
+		
+        location ~ /phpmyadmin {
+            proxy_pass http://$IP:$PMAPORT;
+            proxy_set_header Host \$host;
+        }
+}" > /etc/nginx/conf.d/react.conf
