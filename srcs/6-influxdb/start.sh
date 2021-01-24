ee389bc944ff017ce0a259f52351575d25fb1461
Alexandre Hallaine <ahallain@student.42.fr>
2021-01-24 21:11:47 +0100

    update: influx add telegraf config

--- srcs/6-influxdb/start.sh
+++ srcs/6-influxdb/start.sh
@@ -3,9 +3,8 @@ while ! nc -z localhost 8086 </dev/null
 do sleep 0.1
 done
 /influxdb/influx setup -f -u $INFLUX_USER -p $INFLUX_PASSWD -o $INFLUX_ORG -b $INFLUX_BUCKET -t $INFLUX_TOKEN
-#telegraf config | /influxdb/influx telegrafs create
-/influxdb/influx telegrafs create -n "Example Telegraf config" -d "This is a
- description for an example Telegraf configuration." -f /etc/telegraf.conf
-id=$(/influxdb/influx telegrafs --hide-headers | cut -d$'\t' -f1)
-telegraf --config http://localhost:8086/api/v2/telegrafs/$id &
-
+bucket_id=$(/influxdb/influx bucket list | grep $INFLUX_BUCKET | cut -d$'\t' -f1)
+telegraf_id=$(telegraf config | /influxdb/influx telegrafs create --hide-headers | cut -d$'\t' -f1)
+export INFLUX_TOKEN=$(/influxdb/influx auth create --read-bucket $bucket_id --read-telegrafs --write-telegrafs --hide-headers | cut -d$'\t' -f3)
+echo $INFLUX_TOKEN
+telegraf --config http://localhost:8086/api/v2/telegrafs/$telegraf_id
