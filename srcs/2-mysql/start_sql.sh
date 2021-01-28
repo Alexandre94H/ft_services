fc8d22c56e4bfe1fc07165f088906552dbf80a86
Alexandre Hallaine <ahallain@student.42.fr>
2021-01-28 10:47:48 +0100

    add: mysql persistent volume

--- srcs/2-mysql/start_sql.sh
+++ srcs/2-mysql/start_sql.sh
@@ -1,3 +1,5 @@
+/etc/init.d/mariadb setup
+/etc/init.d/mariadb start
 echo "CREATE USER '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWD';
 GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';
 FLUSH PRIVILEGES" | mysql
